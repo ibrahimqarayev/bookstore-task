581cbe2748f38c933b3a558c5ea077286bda6a23
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-05 23:50:57 +0400

    Initial commit

---
+++ src/test/java/az/ibrahim/bookstoretask/BookstoreTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ibrahim.bookstoretask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
