21b65e0b34a837bfc69730d3701924730f671122
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-11-07 23:46:09 +0400

    Add AuthenticationResponse.java

---
+++ src/main/java/az/ibrahim/bookstoretask/dto/response/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package az.ibrahim.bookstoretask.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthenticationResponse {
+    private String token;
+}
